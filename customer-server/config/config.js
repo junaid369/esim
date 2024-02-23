55e5bbb87e2f3d06c41ac7ccb10f6e461e193a52
muhammedjunaid <junaidmuhammedmj@gmail.com>
2024-02-23 19:36:32 +0400

    expose files are removed

--- customer-server/config/config.js
+++ customer-server/config/config.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 
 module.exports = {
   //Live  DB
-  DATABASE_NAME: "isapxray_db",
-  CONNECTION_URL: "mongodb+srv://baqalath:bMP5OsI0znIqiYOl@cluster0.tbm8gxh.mongodb.net/isapxray_db?retryWrites=true&w=majority",
-  PORT: 4001,
-  USER_COLLECTION:"cln_user",
-ADMIN_LOGIN_COLLECTION:"cln_login",
-  JWT_SECRET: "xray1?%$$is%$",
+  DATABASE_NAME: process.env.DATABASE_NAME,
+  CONNECTION_URL: process.env.CONNECTION_URL,
+  PORT: process.env.PORT,
+  USER_COLLECTION: process.env.USER_COLLECTION,
+  ADMIN_LOGIN_COLLECTION: process.env.USER_LOGIN_COLLECTION,
+  JWT_SECRET: process.env.JWT_SECRET,
 };
